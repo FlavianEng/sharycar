f41af45edb17f07f2ae0aef6d8c8694d14879a19
flavian.eng <engevinflavian@gmail.com>
2021-03-09 11:55:48 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+// import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
+// eslint-disable-next-line no-unused-vars
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
