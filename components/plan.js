da002701fbe5e84524d0e61b507089fa87328864
flavian.eng <engevinflavian@gmail.com>
2021-04-03 18:47:58 +0200

    Fixing UI card

--- components/plan.js
+++ components/plan.js
@@ -112,7 +112,7 @@ export default function Plan() {
       {/* Full plan */}
       <div className="w-72 h-96 bg-white rounded-md shadow-lg flex flex-col justify-between items-center px-6 py-3 flex-grow-0 flex-shrink-0">
         <h2 className="font-medium text-wildStrawberry text-2xl px-4 py-2">
-          Free
+          Full
         </h2>
         <div className="w-24">
           <Image
