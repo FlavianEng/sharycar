c2c376f1c15bc9068b79ff1d08b7c5010f5e7412
flavian.eng <engevinflavian@gmail.com>
2021-04-04 12:41:52 +0200

    fix switch ui on mobile

--- components/switchButton.js
+++ components/switchButton.js
@@ -11,18 +11,18 @@ export default function SwitchButton({ switchId }) {
         key={switchId}
         checked={isOn}
         onChange={() => setIsOn(!isOn)}
-        className="h-0 w-0 invisible"
+        className="h-0 w-0 invisible appearance-none"
       />
       <label
         htmlFor={switchId}
         className={`${
           isOn ? 'bg-wildStrawberry' : 'bg-caribbeanGreen'
-        } relative flex items-center px-2 flex-shrink-0 h-14 transition-colors duration-200 ease-in-out border-0 rounded-2xl cursor-pointer w-36 focus:outline-none focus:shadow-outline`}
+        } relative flex items-center px-2 flex-shrink-0 h-14 transition-colors duration-200 ease-in-out border-0 rounded-2xl cursor-pointer w-36 focus:outline-none focus:shadow-outline appearance-none`}
       >
         <span
           className={` ${
             isOn ? 'translate-x-20' : 'translate-x-0'
-          } inline-block w-12 h-12 transition duration-200 ease-in-out transform bg-white shadow-sm rounded-xl`}
+          } inline-block w-12 h-12 transition duration-200 ease-in-out transform bg-white shadow-sm rounded-xl appearance-none`}
         />
       </label>
     </>
